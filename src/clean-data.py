4a45123086a03cca792ca12da96082a893cefbde
Oli <135059659+lam-oli@users.noreply.github.com>
2024-03-20 20:43:11 -0700

    Results script and other edits (#28)
    * Results script and other script edits
    
    * Questions for classification ipynb and clean-data
    
    * Edited cleaned script
    
    * fix data reference
    
    * fix data cleaning
    
    * fix image generation
    
    ---------
    
    Co-authored-by: Aron Bahram <aron.bahram@gmail.com>

--- src/clean-data.py
+++ src/clean-data.py
@@ -2,26 +2,41 @@ import click
 import pandas as pd
 
 @click.command()
-@click.argument('input_file', type=str)
-@click.argument('output_file', type=str)
+@click.option('--input_file', type=str, default="data/processed/planet-systems.csv")
+@click.option('--output_file', type=str, default="data/processed/planet-systems.csv")
 def clean_data(input_file, output_file):
     """
     Reads data from the input file, performs data cleaning, and saves the cleaned data to the output file.
     """
     # Read the data from the input file
-    data = pd.read_csv(input_file)
+    exoplanet_data = pd.read_csv(input_file)
+
+    # iterate through the columns and rename if "str" is at the end of the column name
+    for col in exoplanet_data.columns:
+        if col.endswith('str'):
+            new_col_name = col[:-3]  # remove "str"
+            exoplanet_data.rename(columns={col: new_col_name}, inplace=True)
     
-    # Check for and drop duplicate rows
-    duplicates = data[data.duplicated()]
-    data.drop_duplicates(inplace=True)
+    # Drop the rows with NA values
+    only_stars_data = exoplanet_data.dropna(subset = ['st_spectype',
+                                                  'sy_umag',
+                                                  'sy_gmag',
+                                                  'sy_rmag',
+                                                  'sy_imag',
+                                                  'sy_zmag'])
     
-    # Drop the 'OperatingSystems' column
-    data.drop(columns=["OperatingSystems"], inplace=True)
-    
+    # Created column names with proper spectral type
+    only_stars_data_copy = only_stars_data.copy() # needed to avoid warning raised by pandas
+    only_stars_data_copy["st_spectype"] = only_stars_data_copy['st_spectype'].transform(lambda x: x[0])
+    only_stars_data_copy = only_stars_data_copy.loc[only_stars_data_copy['st_spectype'].isin(['O','B', 'A', 'F', 'G', 'K', 'M'])]
+    only_stars_data_copy["st_spectype"] = only_stars_data_copy["st_spectype"].astype("category")
+
     # Save the cleaned data to the output file
-    data.to_csv(output_file, index=False)
+    only_stars_data_copy.to_csv(output_file, index=False)
     
-    click.echo("Data cleaned and saved to:", output_file)
+    print("Data cleaned and saved to:", output_file)
 
 if __name__ == "__main__":
     clean_data()
+
+# Are we dropping NAs???
