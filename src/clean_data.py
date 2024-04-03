58a6d56ffba5f62a12eef29377cebb11525086e5
lucyliuyihong <109768159+lucyliuyihong@users.noreply.github.com>
2024-04-02 23:48:20 -0700

    Created functions for the data cleaning script, along with an initial version of the test files for these functions. (#62)
    * created functions for the cleaning and preprocessing script and first draft of their tests
    
    * keeps clean_confidence_intervals function and completed tests for it
    
    * fix import path
    
    * import missing numpy
    
    * fixed failed tests
    
    * deleted pycache
    
    * improve code style
    
    * undo changes to read_data
    
    ---------
    
    Co-authored-by: Aron Bahram <aron.bahram@gmail.com>

--- src/clean_data.py
+++ src/clean_data.py
@@ -2,51 +2,62 @@ import os
 from pathlib import Path
 import click
 import pandas as pd
+import sys
+import os
+
+# Import clean_confidence_intervals function from the src folder
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from clean_confidence_intervals import clean_confidence_intervals
 
 
 @click.command()
 @click.option("--input_file", type=str, default="data/raw/planet-systems.csv")
 @click.option("--output_file", type=str, default="data/processed/planet-systems.csv")
+
+
 def clean_data(input_file, output_file):
     """
-    Reads data from the input file, performs data cleaning, and saves the cleaned data 
-    to the output file.
+    Reads data from the input file, performs data cleaning and preprocessing, 
+    and saves the cleaned data to the output file.
     """
     print("### Cleaning and preprocessing dataset... ###")
     os.makedirs(Path(output_file).parent, exist_ok=True)
-    # Read the data from the input file
+
+    # Reads data from the input file
     exoplanet_data = pd.read_csv(input_file)
 
-    # 1) remove confidence interval from all cells, just keep the mean value for these
-    exoplanet_data = exoplanet_data.apply(lambda col: col.str.split("&").str[0])
-
-    # iterate through the columns and rename if "str" is at the end of the column name
-    for col in exoplanet_data.columns:
-        if col.endswith("str"):
-            new_col_name = col[:-3]  # remove "str"
-            exoplanet_data.rename(columns={col: new_col_name}, inplace=True)
-
     # Drop the rows with NA values
-    only_stars_data = exoplanet_data.dropna(
+    exoplanet_data = exoplanet_data.dropna(
         subset=["st_spectype", "sy_umag", "sy_gmag", "sy_rmag", "sy_imag", "sy_zmag"]
     )
 
-    # Created column names with proper spectral type
-    only_stars_data_copy = (
-        only_stars_data.copy()
+    # Removes confidence intervals, keeping only the mean value (functions imported from src folder)
+    exoplanet_data = clean_confidence_intervals(exoplanet_data)
+    
+    # Iterates through the columns and renames columns by removing 'str' from the end of column names.
+    for col in exoplanet_data.columns:
+        if col.endswith("str"):
+            new_col_name = col[:-3]  # removes "str"
+            exoplanet_data.rename(columns={col: new_col_name}, inplace=True)
+
+    # Filters the data to keep only the rows where the values in the "st_spectype" column are among 
+    # the specified spectral types: "O", "B", "A", "F", "G", "K", or "M" and converts spectral type 
+    # column to category type.
+    exoplanet_data = (
+        exoplanet_data.copy()
     )  # needed to avoid warning raised by pandas
-    only_stars_data_copy["st_spectype"] = only_stars_data_copy["st_spectype"].transform(
+    exoplanet_data["st_spectype"] = exoplanet_data["st_spectype"].transform(
         lambda x: x[0]
     )
-    only_stars_data_copy = only_stars_data_copy.loc[
-        only_stars_data_copy["st_spectype"].isin(["O", "B", "A", "F", "G", "K", "M"])
+    exoplanet_data = exoplanet_data.loc[
+        exoplanet_data["st_spectype"].isin(["O", "B", "A", "F", "G", "K", "M"])
     ]
-    only_stars_data_copy["st_spectype"] = only_stars_data_copy["st_spectype"].astype(
+    exoplanet_data["st_spectype"] = exoplanet_data["st_spectype"].astype(
         "category"
     )
-
-    # Save the cleaned data to the output file
-    only_stars_data_copy.to_csv(output_file, index=False)
+    
+    # Saves the cleaned and preprocessed data to the output file
+    exoplanet_data.to_csv(output_file, index=False)
 
     print("Data cleaned and saved to:", output_file)
 
