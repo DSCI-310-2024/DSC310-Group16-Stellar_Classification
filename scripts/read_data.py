06342d04278e7474b13dfb3a8a1fed9255842a75
Aron Bahram <aron.bahram@gmail.com>
2024-04-12 12:17:37 -0700

    introduce columns as parameter in fetch_data (#84)

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -1,74 +1,8 @@
 import os
-from pathlib import Path
 
 import click
-import pandas as pd
-import requests
 
-
-def fetch_data(
-    url: str,
-    output_path: str,
-) -> pd.DataFrame:
-    """Download dataset from the specified url and save it to the provided path.
-
-    The dataset is saved under data/raw/Y-M-D_planet-systems.csv,
-    along with its processed version under data/processed/planet-systems.csv by default.
-
-    Documentation for constructing a TPA call to retrieve the dataset:
-    https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=PS
-
-    Args:
-        url (str): The URL from which to fetch the data.
-
-        output_path (str): The file path where the fetched raw data will be saved.
-
-    Returns:
-        pd.DataFrame: A pandas DataFrame containing the fetched data.
-
-    """
-    dataset_name = "planet-systems"
-    raw_data_dir = Path("data") / "raw"
-    default_out_path = raw_data_dir / f"{dataset_name}.csv"
-    raw_data_path = output_path or default_out_path
-
-    # make directory where we store our raw data
-    os.makedirs(raw_data_dir, exist_ok=True)
-
-    # download the raw data as CSV under the raw data directory using a TPA query
-    base_url = "https://exoplanetarchive.ipac.caltech.edu"
-    columns = (
-        "pl_name,"
-        "st_spectype,"
-        "sy_umag,"
-        "sy_gmag,"
-        "sy_rmag,"
-        "sy_imag,"
-        "sy_zmag"
-    )
-    query = f"select+{columns}+from+ps"
-    format = "csv"
-    url = url or f"{base_url}/TAP/sync?query={query}&format={format}"
-
-    print(f"Downloading Planet Systems dataset from {url}\nunder {raw_data_path}")
-
-    try:
-        response = requests.get(url)
-        raw_data = response.content
-    except requests.exceptions.RequestException:
-        print(f"ERROR: Error while trying to download the dataset from {url}")
-        raise
-
-    # write downloaded content into a file under the raw data directory
-    with open(raw_data_path, "wb") as f:
-        f.write(raw_data)
-
-    # df holds the expolanet dataset as a DataFrame object
-    df = pd.read_csv(raw_data_path)
-
-    print(f"Successfully loaded dataset from {raw_data_path}")
-
-    return df
+from src.fetch_exoplanet_dataset import fetch_data
 
 
 @click.command()
@@ -77,7 +11,13 @@ def fetch_data(
 def read_data(url, output_path):
     print(f"### Running {os.path.basename(__file__)} ###")
 
-    exoplanet_data = fetch_data(url, output_path)
+    # define the columns we want do keep in the dataset
+    columns = (
+        "pl_name," "st_spectype," "sy_umag," "sy_gmag," "sy_rmag," "sy_imag," "sy_zmag"
+    )
+
+    # download the dataset
+    exoplanet_data = fetch_data(url=url, output_path=output_path, columns=columns)
 
     print(f"### Successfully ran {os.path.basename(__file__)} ###")
     return exoplanet_data
--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -7,14 +7,17 @@ import pytest
 import requests
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from src.read_data import fetch_data
+from scripts.read_data import fetch_data
 
 
 @pytest.fixture
-def url():
-    base_url = "https://exoplanetarchive.ipac.caltech.edu"
-    columns = "pl_name,st_spectype"
-    return f"{base_url}/TAP/sync?query=select+{columns}+from+ps&format=csv"
+def base_url():
+    return "https://exoplanetarchive.ipac.caltech.edu"
+
+
+@pytest.fixture
+def columns():
+    return ["pl_name", "st_spectype"]
 
 
 @pytest.fixture
@@ -22,36 +25,40 @@ def output_path():
     return "data/raw/test_data.csv"
 
 
-def test_fetch_data_downloads_data(url, output_path):
-    df = fetch_data(url, output_path)
+def test_fetch_data_downloads_data(base_url, output_path, columns):
+    df = fetch_data(base_url, output_path, columns)
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
     assert os.path.exists(output_path)
 
 
-def test_fetch_data_raises_exception_for_invalid_url(url, output_path):
-    url = "invalid_url"
+def test_fetch_data_raises_exception_for_invalid_url(base_url, output_path, columns):
+    base_url = "invalid_url"
     with pytest.raises(requests.exceptions.RequestException):
-        fetch_data(url, output_path)
+        fetch_data(base_url, output_path, columns)
 
 
-def test_fetch_data_saves_data_to_specified_output_path(url, output_path):
+def test_fetch_data_saves_data_to_specified_output_path(base_url, output_path, columns):
     output_path = "data/raw/custom_test_data.csv"
-    fetch_data(url, output_path)
+    fetch_data(base_url, output_path, columns)
     assert os.path.exists(output_path)
 
 
-def test_fetch_data_default_output_path(url, output_path):
-    fetch_data(url, None)
+def test_fetch_data_default_output_path(base_url, columns):
+    fetch_data(base_url, None, columns)
     default_output_path = Path("data/raw/planet-systems.csv")
     assert os.path.exists(default_output_path)
 
 
-def test_fetch_data_response_is_dataframe(url, output_path):
-    df = fetch_data(url, output_path)
+def test_fetch_data_response_is_dataframe(base_url, output_path, columns):
+    df = fetch_data(base_url, output_path, columns)
     assert isinstance(df, pd.DataFrame)
 
 
-def test_fetch_data_downloads_data_from_default_url(url, output_path):
-    fetch_data(None, output_path)
+def test_fetch_data_downloads_data_from_default_url(output_path, columns):
+    fetch_data(None, output_path, columns)
     assert os.path.exists(output_path)
+
+def test_fetch_data_returns_df_with_columns_that_are_given(output_path, columns):
+    df = fetch_data(None, output_path, columns)
+    assert list(df.columns) == columns
