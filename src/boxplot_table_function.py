082a1130d50bd3af890245f56b62c9b949283a6c
Lucy Liu <lucyliuyihong@gmail.com>
2024-04-12 19:44:09 -0700

    added consistant documentation for all function scripts

--- src/boxplot_table_function.py
+++ src/boxplot_table_function.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 
 def make_boxplot_and_table(data, column_name, csv_dir, box_plot_dir):
     """
-    Purpose: creates a boxplot and a csv file given the dataframe the desired column name
+    Purpose: 
+    creates a boxplot and a csv file given the dataframe the desired column name
     of which we want to investigate. Our function will also save all of these figures to the given
     file directory path. 
 
-    Requirements: Must have a column called st_spectype to group by. This is project specific in our
+    Requirements: 
+    Must have a column called st_spectype to group by. This is project specific in our
     case and can be easily altered if using other data.
 
     Parameters:
@@ -16,9 +18,9 @@ def make_boxplot_and_table(data, column_name, csv_dir, box_plot_dir):
     - box_plot_dir: Path to the directory in which we want the box plot figures to live
 
     Returns:
-    1. A csv with the quantitative descriptions (mean, std, min) for the four bands (FGKM) loaded into
+    - A csv with the quantitative descriptions (mean, std, min) for the four bands (FGKM) loaded into
     the corresponding directory
-    2. A boxplot of each of the four bands loaded into the corresponding directory
+    - A boxplot of each of the four bands loaded into the corresponding directory
     """
 
     #create and save csv for given column provided
